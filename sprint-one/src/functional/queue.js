a140833279605823e66d762790bf129db04ce9af
Brendon Verissimo <bverissimo@ucdavis.edu>
2014-01-08 12:29:38 -0800

    Queue has size method and stores new items

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,20 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[size] = value;
+    size++;
   };
 
   instance.dequeue = function(){
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
