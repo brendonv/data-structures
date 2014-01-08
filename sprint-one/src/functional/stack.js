18460ebee1aeaeadc33cac0937587ef4af637850
Brendon Verissimo <bverissimo@ucdavis.edu>
2014-01-08 12:14:51 -0800

    Returns correct size of stack after adding items

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,21 +1,25 @@
 var makeStack = function(){
-    var instance = {};
+  var instance = {};
 
   // Use an object with numeric keys to store values
-    var storage = {};
+  var storage = {};
+  var size = 0;
   /* START PROMPT
     var size; // Hint: set an initial value here
   -END PROMPT */
 
   // Implement the methods below
   instance.push = function(value){
+    size++;
   };
 
   instance.pop = function(){
+    size--;
   };
 
   instance.size = function(){
+    return size;
   };
   
-    return instance;
+  return instance;
   };
