126cf1cd2ecf21d36cde5751dd91b0d45f7eace6
Brendon Verissimo <bverissimo@ucdavis.edu>
2014-01-08 12:25:59 -0800

    Stack returns items sequentially (LIFO)

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -10,11 +10,17 @@ var makeStack = function(){
 
   // Implement the methods below
   instance.push = function(value){
+    storage[size] = value;
     size++;
   };
 
   instance.pop = function(){
-    size--;
+    if(size) {
+      size--;
+      var result = storage[size];
+      delete storage[size];
+      return result;
+    }
   };
 
   instance.size = function(){
