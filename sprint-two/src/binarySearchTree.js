ee0e54943ba15e61491955ca8421a0378d3a872e
Hack Reactor Students <bverissimo@ucdavis.edu>
2014-01-10 18:42:30 -0800

    working on hash extra credit

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,70 @@
 var makeBinarySearchTree = function(value){
+  var node = Object.create(methods);
+  node.value = value;
+  node.left = {};
+  node.right = {};
+  return node;
+};
+
+var methods = {};
+
+methods.insert = function(val) {
+  var node = makeBinarySearchTree(val);
+
+  var recurse = function(element) { 
+    if (val < element.value) {
+      if (element.left.value) {
+        return recurse(element.left);
+      } else {
+        element.left = node;
+      }
+
+    } else {
+      if (element.right.value) {
+        return recurse(element.right);
+      } else {
+        element.right = node;
+      }
+    }
+  };
+
+  recurse(this);
+};
+
+methods.contains = function(val) {
+  var matched = false;
+
+  var recurse = function(element) { 
+    if (val < element.value) {
+      if (element.left.value === val) matched = true;
+      if (element.left.value) {
+        recurse(element.left);
+      } 
+    } else {
+      if (element.right.value === val) matched = true;
+      if (element.right.value) {
+        recurse(element.right);
+      }
+    }
+  };
+
+  recurse(this);
+  return matched;
+};
+
+methods.depthFirstLog = function(func) {
+
+  var recurse = function(element) { 
+    if (element.left.value) {
+      recurse(element.left);
+    }
+
+    if (element.right.value) {
+      recurse(element.right);
+    }
+
+    element.value = func(element.value);
+  };
+
+  recurse(this);
 };
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,22 +1,50 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
-  // console.log(makeLimitedArray());
-  //console.log(this, this._storage);
+  this._count = 0;
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[i] = {k: v};
+  var size = this._count / this._limit;
+  if (size >= 0.75) this.rehash("increase");
+  this._storage[i] = [];
+  this._storage[i][0] = k;
+  this._storage[i][1] = v;
+  this._count++;
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage[i].k;
+  return this._storage[i][1];
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[i].k = null;
+  this._storage[i][1] = null;
+  var size = this._count / this._limit;
+  if (size <= 0.25) this.rehash();
+  this._count--;
+};
+
+HashTable.prototype.rehash = function(size) {
+  var memo = [];
+  for(var data in this._storage) {
+    if(typeof this._storage[data] === 'object') {
+      memo.push([this._storage[data][0], this._storage[data][1]]);
+    } 
+  }
+
+  if (size === "increase") {
+    this._limit *= 2;
+  } else {
+    this._limit /= 2;
+  }
+  this._storage = makeLimitedArray(this._limit);
+  this._count = 0;
+
+  for(var j = 0; j < memo.length; j++) {
+    this.insert(memo[j][0], memo[j][1]);
+  }
 };
 
